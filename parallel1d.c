57015e95b1d3bf9af9df49c356a21f55ac2182e0
Diana Plesa <diana.plesa04@gmail.com>
2026-01-08 15:50:12 +0200

    modified grid format in memory to be continuous

--- parallel1d.c
+++ parallel1d.c
@@ -4,19 +4,7 @@
 #include <time.h>
 #include <mpi.h>
 
-char** allocate_grid(int *rows, int *cols)
-{
-    char** grid = malloc(sizeof(char*) * (*rows));
-
-    for (int i = 0; i < *rows; i++)
-    {
-        grid[i] = malloc(sizeof(char) * (*cols));
-    }
-
-    return grid;
-}
-
-char** get_grid(char* filename, int* rows, int* cols)
+char* get_grid(char* filename, int* rows, int* cols)
 {
     FILE *f = fopen(filename, "r");
 
@@ -28,13 +16,13 @@ char** get_grid(char* filename, int* rows, int* cols)
 
     fscanf(f, "%d%d", rows, cols);
 
-    char** grid = allocate_grid(rows, cols);
+    char* grid = malloc((*rows) * (*cols) * sizeof(char));
 
     for (int i = 0; i < *rows; i++)
     {
         for (int j = 0; j < *cols; j++)
         {
-            fscanf(f, " %c", &grid[i][j]);
+            fscanf(f, " %c", &grid[i * (*cols) + j]);
         }
     }
 
@@ -47,107 +35,107 @@ char** get_grid(char* filename, int* rows, int* cols)
     return grid;
 }
 
-void print_grid(char** grid, int rows, int cols)
+void print_grid(char* grid, int rows, int cols)
 {
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
         {
-            printf("%c ", grid[i][j]);
+            printf("%c ", grid[i * cols + j]);
         }
         printf("\n");
     }
 }
 
-int is_bacteria(char** grid, int i, int j)
-{
-    if (grid[i][j] == 'X') return 1;
-    return 0;
-}
+// int is_bacteria(char* grid, int i, int j)
+// {
+//     if (grid[i * colsj] == 'X') return 1;
+//     return 0;
+// }
 
-int get_nb_bacteria_around(char** grid, int src_i, int src_j, int rows, int cols)
-{
-    int nb_bacteria = 0;
+// int get_nb_bacteria_around(char** grid, int src_i, int src_j, int rows, int cols)
+// {
+//     int nb_bacteria = 0;
 
-    for (int i = -1; i <= 1; i++)
-    {
-        for (int j = -1; j <= 1; j++)
-        {
-            if (i == 0 && j == 0) continue; //skip source
+//     for (int i = -1; i <= 1; i++)
+//     {
+//         for (int j = -1; j <= 1; j++)
+//         {
+//             if (i == 0 && j == 0) continue; //skip source
 
-            int actual_i = i + src_i;
-            int actual_j = j + src_j;
+//             int actual_i = i + src_i;
+//             int actual_j = j + src_j;
 
-            if (actual_i >= 0 && actual_i < rows && actual_j >= 0 && actual_j < cols)
-            {
-                if (grid[actual_i][actual_j] == 'X') nb_bacteria++;
-            }
-        }
-    }
+//             if (actual_i >= 0 && actual_i < rows && actual_j >= 0 && actual_j < cols)
+//             {
+//                 if (grid[actual_i][actual_j] == 'X') nb_bacteria++;
+//             }
+//         }
+//     }
 
-    return nb_bacteria;
-}
+//     return nb_bacteria;
+// }
 
-void bacterium_death(char** grid, int i, int j)
-{
-    grid[i][j] = '.';
-}
+// void bacterium_death(char** grid, int i, int j)
+// {
+//     grid[i][j] = '.';
+// }
 
-void spawn_bacterium(char** grid, int i, int j)
-{
-    grid[i][j] = 'X';
-}
+// void spawn_bacterium(char** grid, int i, int j)
+// {
+//     grid[i][j] = 'X';
+// }
 
-void update_grid_point(char** grid, char** next_grid, int i, int j, int rows, int cols)
-{
-    int nb_bacteria = get_nb_bacteria_around(grid, i, j, rows, cols);
-    if (is_bacteria(grid, i, j)) 
-    {
-        if (nb_bacteria < 2 || nb_bacteria > 3) bacterium_death(next_grid, i, j);
-        else next_grid[i][j] = grid[i][j];
-    }
-    else if (!is_bacteria(grid, i, j)) 
-    {
-        if (nb_bacteria == 3) spawn_bacterium(next_grid, i, j);
-        else next_grid[i][j] = grid[i][j];
-    }
-}
+// void update_grid_point(char** grid, char** next_grid, int i, int j, int rows, int cols)
+// {
+//     int nb_bacteria = get_nb_bacteria_around(grid, i, j, rows, cols);
+//     if (is_bacteria(grid, i, j)) 
+//     {
+//         if (nb_bacteria < 2 || nb_bacteria > 3) bacterium_death(next_grid, i, j);
+//         else next_grid[i][j] = grid[i][j];
+//     }
+//     else if (!is_bacteria(grid, i, j)) 
+//     {
+//         if (nb_bacteria == 3) spawn_bacterium(next_grid, i, j);
+//         else next_grid[i][j] = grid[i][j];
+//     }
+// }
 
-void free_grid(char** grid, int rows)
-{
-    for (int i = 0; i < rows; i++)
-    {
-        free(grid[i]);
-    }
+// void free_grid(char** grid, int rows)
+// {
+//     for (int i = 0; i < rows; i++)
+//     {
+//         free(grid[i]);
+//     }
     
-    free(grid);
-}
+//     free(grid);
+// }
 
-void write_to_output_file(char** grid, int rows, int cols, char* filename)
-{
-    FILE *f = fopen(filename, "w");
+// void write_to_output_file(char** grid, int rows, int cols, char* filename)
+// {
+//     FILE *f = fopen(filename, "w");
 
-    if (f == NULL)
-    {
-        fprintf(stderr, "Error opening file");
-        exit(-1);
-    }
+//     if (f == NULL)
+//     {
+//         fprintf(stderr, "Error opening file");
+//         exit(-1);
+//     }
 
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            fprintf(f, "%c ", grid[i][j]);
-        }
-        fprintf(f, "\n");
-    }
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < cols; j++)
+//         {
+//             fprintf(f, "%c ", grid[i][j]);
+//         }
+//         fprintf(f, "\n");
+//     }
 
-    if (fclose(f) != 0)
-    {
-        fprintf(stderr, "Error opening file");
-        exit(-1);
-    }
-}
+//     if (fclose(f) != 0)
+//     {
+//         fprintf(stderr, "Error opening file");
+//         exit(-1);
+//     }
+// }
 
 int main(int argc, char** argv)
 {
@@ -184,32 +172,32 @@ int main(int argc, char** argv)
     int mode = 1;
 
     int rows = 0, cols = 0;
-    char** grid = get_grid(filename, &rows, &cols);
-    char** next_grid = allocate_grid(&rows, &cols);
+    char* grid = NULL;
+    char* next_grid = NULL;
+
+    if (rank == 0)
+    {
+        grid = get_grid(filename, &rows, &cols);
+        next_grid = malloc(rows * cols * sizeof(char));
+    }
+
+    MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&cols, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int local_rows = rows / size;
+    char* local_grid = malloc((local_rows + 2) * cols * sizeof(char)); //+ 2 for halo rows (upper and lower)
+    char* next_local_grid = malloc((local_rows + 2) * cols * sizeof(char));
+    
+    MPI_Scatter(grid, local_rows * cols, MPI_CHAR, &local_grid[cols], //start at row 1
+        local_rows * cols, MPI_CHAR, 0, MPI_COMM_WORLD);
+    
 
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     for (int i = 0; i < generations; i++)
     {
-        if (mode == 2)
-        {
-            printf("====================\nGENERATION %d\n", i+1);
-            print_grid(grid, rows, cols);
-        }
 
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                update_grid_point(grid, next_grid, i, j, rows, cols);
-            }
-        }
-
-        //swap between grids for memory management efficiency
-        char** temp = grid;
-        grid = next_grid;
-        next_grid = temp;
     }
 
     clock_gettime(CLOCK_MONOTONIC, &end);
@@ -217,12 +205,9 @@ int main(int argc, char** argv)
 
     if (rank == 0)
     {
-        printf("Serial time: %f\n", time_taken);
-        write_to_output_file(next_grid, rows, cols, "out_parallel.txt");
+        printf("Parallel time: %f\n", time_taken);
+        //write_to_output_file(next_grid, rows, cols, "out_parallel.txt");
     }
-
-    free_grid(grid, rows);
-    free_grid(next_grid, rows);
 
     MPI_Finalize();
 
